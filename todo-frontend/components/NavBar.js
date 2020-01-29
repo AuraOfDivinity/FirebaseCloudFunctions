1e75af7c1cba70b0665d2dd5cc57e4126a8d3071
Asel <peirisasel373@gmail.com>
2020-01-29 19:06:31 +0530

    Nav Bar

---
+++ todo-frontend/components/NavBar.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from "reactstrap";
+
+const NavBar = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand href="/">WhatToDo</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">My Todos</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                Add To do
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
